bcec9ac1b6fbd5e07e613622b53afc47031e0b42
NycolasSantana <nycolasrobertods16@gmail.com>
2023-02-12 00:37:15 -0300

    Corrigindo Bugs

--- projeto-dia/assets/js/default.js
+++ projeto-dia/assets/js/default.js
@@ -12,32 +12,15 @@
 */
 
 let localDate = new Date();
-//localDate.getHours();
-let hours = 6
+
 let msg = window.document.getElementById('horas');
 let img = window.document.getElementById('imgHoras');
 let body = window.document.body
 let main = window.document.getElementById('conteudo');
 let footer = window.document.getElementById('rodape');
 
-if (hours >= 18 && hours <= 24) { // Noite
-    console.log('NOITE')
-} else if (hours >= 12 && hours <= 18) { // Tarde
-    console.log('TARDE')
-} else if (hours >= 6 && hours <= 12) { // Manhã
-    msg.innerText += ` ${hours}h da Manhã`;
-    body.style.backgroundColor = '#fce962';
-    main.style.backgroundColor = 'rgb(0, 10, 40)';
-    main.style.color = 'rgb(233, 233, 233)';
-    footer.style.color = 'rgb(233, 233, 233)';
-    footer.style.backgroundColor = 'rgb(25, 25, 25)';
-    img.style.backgroundImage = "url('./assets/images/manhã.jpg')";
-    img.style.backgroundPosition = 'center center';
-    img.style.backgroundRepeat = 'no-repeat';
-    img.style.backgroundAttachment = 'local';
-    img.style.backgroundSize = 'cover';
-} else if (hours >= 1 && hours <= 6) { // Madrugada
-    msg.innerText += ` ${hours}h da Madrugada`;
+if (localDate.getHours() >= 0 && localDate.getHours() <= 5) { // Madrugada
+    msg.innerText += ` ${localDate.getHours()}h da Madrugada`;
     body.style.backgroundColor = 'rgb(15, 15, 15)';
     main.style.backgroundColor = 'rgb(20, 20, 20)';
     main.style.color = 'rgb(233, 233, 233)';
@@ -48,6 +31,52 @@ if (hours >= 18 && hours <= 24) { // Noite
     img.style.backgroundRepeat = 'no-repeat';
     img.style.backgroundAttachment = 'local';
     img.style.backgroundSize = 'cover';
-} else {
-    body.innerHTML = 'ERRO DE CÓDIGO ou HORÁRIO INVÁLIDO' // Caso o horário seja inválido
+} else if (localDate.getHours() >= 5 && localDate.getHours() < 12) { // Manhã
+    msg.innerText += ` ${localDate.getHours()}h da Manhã`;
+    body.style.backgroundColor = '#fce962';
+    main.style.backgroundColor = 'rgb(0, 10, 40)';
+    main.style.color = 'rgb(233, 233, 233)';
+    footer.style.color = 'rgb(233, 233, 233)';
+    footer.style.backgroundColor = 'rgb(25, 25, 25)';
+    img.style.backgroundImage = "url('./assets/images/manhã.jpg')";
+    img.style.backgroundPosition = 'center center';
+    img.style.backgroundRepeat = 'no-repeat';
+    img.style.backgroundAttachment = 'local';
+    img.style.backgroundSize = 'cover';
+} else if (localDate.getHours() >= 12 && localDate.getHours() < 18) { // Tarde
+    msg.innerText += ` ${localDate.getHours()}h da Tarde`;
+    body.style.backgroundColor = '#fce962';
+    main.style.backgroundColor = 'rgb(0, 10, 40)';
+    main.style.color = 'rgb(233, 233, 233)';
+    footer.style.color = 'rgb(233, 233, 233)';
+    footer.style.backgroundColor = 'rgb(25, 25, 25)';
+    img.style.backgroundImage = "url('./assets/images/tarde.jpg')";
+    img.style.backgroundPosition = 'center center';
+    img.style.backgroundRepeat = 'no-repeat';
+    img.style.backgroundAttachment = 'local';
+    img.style.backgroundSize = 'cover';
+} else if (localDate.getHours() >= 0 && localDate.getHours() <= 6) { // Noite
+    msg.innerText += ` ${localDate.getHours()}h da Noite`;
+    body.style.backgroundColor = 'rgb(15, 15, 15)';
+    main.style.backgroundColor = 'rgb(20, 20, 20)';
+    main.style.color = 'rgb(233, 233, 233)';
+    footer.style.color = 'rgb(233, 233, 233)';
+    footer.style.backgroundColor = 'rgb(25, 25, 25)';
+    img.style.backgroundImage = "url('./assets/images/noite.jpg')";
+    img.style.backgroundPosition = 'center center';
+    img.style.backgroundRepeat = 'no-repeat';
+    img.style.backgroundAttachment = 'local';
+    img.style.backgroundSize = 'cover';
+} else { // Noite
+    msg.innerText += ` ${localDate.getHours()}h da Noite`;
+    body.style.backgroundColor = 'rgb(15, 15, 15)';
+    main.style.backgroundColor = 'rgb(20, 20, 20)';
+    main.style.color = 'rgb(233, 233, 233)';
+    footer.style.color = 'rgb(233, 233, 233)';
+    footer.style.backgroundColor = 'rgb(25, 25, 25)';
+    img.style.backgroundImage = "url('./assets/images/noite.jpg')";
+    img.style.backgroundPosition = 'center center';
+    img.style.backgroundRepeat = 'no-repeat';
+    img.style.backgroundAttachment = 'local';
+    img.style.backgroundSize = 'cover';
 }
