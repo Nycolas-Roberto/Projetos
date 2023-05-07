85e84c444ee06021a7055c1f9dd06a0e700b318b
Nycolas-Roberto <nycolasrobertods16@gmail.com>
2023-05-07 19:23:15 -0300

    Projeto gerador de Senhas

---
+++ gerenciador de senhas/assets/js/gerar.js
@@ -0,0 +1,73 @@
+function gerarSenha(tamanho, palavraChave) {
+    var caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_-+=,.?/:";
+    let senha = palavraChave + '=====' + '';
+    for (var i = 0; i < tamanho; i++) {
+        senha += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    }
+    return senha
+}
+
+
+function checarForcaSenha(senha) {
+    let forca = 0;
+
+    // verifica se a senha tem pelo menos 8 caracteres
+    if (senha.length >= 8) {
+        forca++;
+    }
+
+    // verifica se a senha tem letras maiúsculas e minúsculas
+    if (/[a-z]/.test(senha) && /[A-Z]/.test(senha)) {
+        forca++;
+    }
+
+    // verifica se a senha tem números
+    if (/\d/.test(senha)) {
+        forca++;
+    }
+
+    // verifica se a senha tem caracteres especiais
+    if (/[@$!%*?&]/.test(senha)) {
+        forca++;
+    }
+
+    // determina a força da senha com base nas regras
+    if (forca === 4) {
+        return 100
+    } else if (forca >= 2) {
+        return 50
+    } else {
+        return 25
+    }
+}
+
+
+let btn = document.querySelector('#btn_generator');
+btn.addEventListener("click", function () {
+    let size = document.querySelector("#sizePass").value;
+    let fonte = document.querySelector("#fonte").value;
+    let palavraChave = document.querySelector('#palavraChave').value;
+    if (size >= 8 && palavraChave.length >= 3 && fonte.length >= 12) {
+        let outputPass = document.querySelector("#outputPass");
+        senhaGerada = gerarSenha(size, palavraChave);
+        outputPass.value = senhaGerada;
+
+        let forte = checarForcaSenha(senhaGerada);
+
+
+        let jsonSenha = {
+            senha: senhaGerada,
+            fonte: fonte,
+            potencia: forte
+        };
+        let jsonSenhas = localStorage.getItem("jsonSenhas");
+        if (jsonSenhas === null) {
+            jsonSenhas = [];
+        } else {
+            jsonSenhas = JSON.parse(jsonSenhas);
+        }
+        jsonSenhas.push(jsonSenha);
+        localStorage.setItem("jsonSenhas", JSON.stringify(jsonSenhas));
+
+    }
+});
---
+++ gerenciador de senhas/assets/js/outlineStorage.js
@@ -0,0 +1,66 @@
+// Recupera a lista de senhas do localStorage
+let senhasUsuario = JSON.parse(localStorage.getItem("jsonSenhas"));
+
+// Seleciona o elemento HTML onde as senhas serão exibidas
+let carouselInner = document.querySelector(".carousel-inner");
+
+// Itera sobre cada item da lista de senhas e cria a estrutura HTML correspondente
+senhasUsuario.forEach(function (item) {
+    // Cria um novo elemento HTML com os valores recuperados
+    let novaSenha = document.createElement("div");
+    novaSenha.className = "carousel-item";
+    if (item === senhasUsuario[0]) {
+        novaSenha.classList.add("active");
+    }
+
+    let card = document.createElement("div");
+    card.className = "card";
+    card.style = "width: 60vw; margin: auto;";
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    let cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    let cardTitleText = document.createTextNode(item.senha);
+    cardTitle.appendChild(cardTitleText);
+
+    let cardSubtitle = document.createElement("h6");
+    cardSubtitle.className = "mb-2";
+    let cardSubtitleLink = document.createElement("a");
+    cardSubtitleLink.href = item.link;
+    cardSubtitleLink.style.textDecoration = "none";
+    let cardSubtitleLinkText = document.createTextNode(item.fonte);
+    cardSubtitleLink.appendChild(cardSubtitleLinkText);
+    cardSubtitle.appendChild(cardSubtitleLink);
+
+    let mt4 = document.createElement("div");
+    mt4.className = "mt-4";
+    let mt4Subtitle = document.createElement("h6");
+    mt4Subtitle.className = "card-subtitle mb-2 text-body-secondary";
+    let mt4SubtitleText = document.createTextNode(`Quão forte a senha é?`);
+    mt4Subtitle.appendChild(mt4SubtitleText);
+    let progress = document.createElement("div");
+    progress.className = "progress";
+    progress.setAttribute("role", "progressbar");
+    progress.setAttribute("aria-label", "Basic example");
+    progress.setAttribute("aria-valuenow", item.potencia);
+    progress.setAttribute("aria-valuemin", "0");
+    progress.setAttribute("aria-valuemax", "100");
+    let progressBar = document.createElement("div");
+    progressBar.className = "progress-bar";
+    progressBar.style.width = `${item.potencia}%`;
+    progress.appendChild(progressBar);
+    mt4.appendChild(mt4Subtitle);
+    mt4.appendChild(progress);
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardSubtitle);
+    cardBody.appendChild(mt4);
+
+    card.appendChild(cardBody);
+    novaSenha.appendChild(card);
+
+    // Adiciona o novo elemento HTML ao seu documento
+    carouselInner.appendChild(novaSenha);
+});
